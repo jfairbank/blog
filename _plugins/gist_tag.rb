f34d5c30d9bd2d0d6a4983acc6616b30b5382459
Jeremy Fairbank <elpapapollo@gmail.com>
2015-01-03 15:14:04 -0500

    Add custom gist liquid tag

---
+++ _plugins/gist_tag.rb
@@ -0,0 +1,42 @@
+# Credit: https://gist.github.com/gsong/1937862#file-gist-rb
+require 'cgi'
+require 'open-uri'
+require 'active_support/cache'
+
+module JF
+  class GistTag < Liquid::Tag
+    def initialize(tag_name, gist_ref, tokens)
+      @gist_ref = gist_ref.strip
+      @gist_id, @filename = @gist_ref.split('/')
+      super
+    end
+
+    # A simple file-based cache in ./_tmp shared by all GistTag instances
+    def cache
+      @@cache ||= ActiveSupport::Cache::FileStore.new("_tmp/gist")
+    end
+
+    # Return from cache if possible; otherwise fetch and add it to cache
+    def fetch_gist(raw_gist_uri)
+      cache.fetch(raw_gist_uri) do
+        open(raw_gist_uri).read.chomp
+      end
+    end
+
+    def render(context)
+      raw_uri = "https://gist.github.com/raw/#{@gist_id}/#{@filename}"
+      script_uri = "https://gist.github.com/#{@gist_id}.js?file=#{@filename}"
+
+      <<MARKUP.strip
+<div id="gist-#{@gist_ref.gsub(/[^a-z0-9]/i,'-')}">
+<script src="#{script_uri}"></script>
+<noscript>
+<pre>#{CGI.escapeHTML(fetch_gist(raw_uri))}</pre>
+</noscript>
+</div>
+MARKUP
+    end
+  end
+end
+
+Liquid::Template.register_tag('gist', JF::GistTag)
